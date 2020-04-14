4bb2a1c8229ad526fd72efb56d446b5baf9425d8
Hugh Brown <hughdbrown@yahoo.com>
2020-04-14 00:04:18 -0600

    Initial revision

---
+++ icalendar.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+from hashlib import sha1
+from datetime import datetime
+from json import loads
+
+
+EVENT_HEAD_FMT = """BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VTIMEZONE
+TZID:{time_zone}
+X-LIC-LOCATION:{time_zone}
+BEGIN:DAYLIGHT
+DTSTART:19700308T020000
+RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU
+TZNAME:MDT
+TZOFFSETFROM:-0700
+TZOFFSETTO:-0600
+END:DAYLIGHT
+BEGIN:STANDARD
+DTSTART:19701101T020000
+RRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU
+TZNAME:MST
+TZOFFSETFROM:-0600
+TZOFFSETTO:-0700
+END:STANDARD
+END:VTIMEZONE"""
+
+EVENT_FMT = """BEGIN:VEVENT
+UID:{hash}
+DTSTART;TZID={time_zone}:{start_time}
+DTEND;TZID={time_zone}:{end_time}
+SUMMARY:{summary}
+END:VEVENT"""
+
+
+def process_event(args):
+    return EVENT_FMT.format(**args)
+
+
+def print_ics(events, tz="America/New_York"):
+    args = {
+        'hash': sha1(str(events)).hexdigest(),
+        'time_zone': tz,
+    }
+    print(EVENT_HEAD_FMT.format(**args))
+    for event in events:
+        extra = {
+            'start_time': event['start_time'],
+            'end_time': event['end_time'],
+            'hash': sha1(str(event)).hexdigest(),
+            'summary': event['summary'],
+        }
+        args.update(extra)
+        print(process_event(args))
+    print("""END:VCALENDAR""")
+
+
+def outco(unformatted_events):
+    datefmt = "%Y%m%dT%H%M%S"
+    return [
+        {
+            "start_time": datetime(*event["start"]).strftime(datefmt),
+            "end_time": datetime(*event["end"]).strftime(datefmt),
+            "summary": event["msg"],
+        }
+        for event in unformatted_events
+    ]
+
+
+if __name__ == '__main__':
+    unformatted = loads(open("events.json").read())
+    events = outco(unformatted)
+    print_ics(events, tz="America/Los_Angeles")
