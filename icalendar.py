a6603973fb656ba33274928ec38a7cd55c7024fc
Hugh Brown <hughdbrown@yahoo.com>
2020-04-14 07:52:04 -0600

    Hack in url for meeting

--- icalendar.py
+++ icalendar.py
@@ -32,11 +32,13 @@ UID:{hash}
 DTSTART;TZID={time_zone}:{start_time}
 DTEND;TZID={time_zone}:{end_time}
 SUMMARY:{summary}
+URL:{url}
+DESCRIPTION:{url}
 END:VEVENT"""
 
 
-def process_event(args):
-    return EVENT_FMT.format(**args)
+def process_event(kwargs):
+    return EVENT_FMT.format(**kwargs)
 
 
 def print_ics(events, tz):
@@ -51,6 +53,7 @@ def print_ics(events, tz):
             'end_time': event['end_time'],
             'hash': sha1(str(event)).hexdigest(),
             'summary': event['summary'],
+            'url': event.get('url', ""),
         }
         args.update(extra)
         print(process_event(args))
@@ -64,6 +67,7 @@ def outco(unformatted_events):
             "start_time": datetime(*event["start"]).strftime(datefmt),
             "end_time": datetime(*event["end"]).strftime(datefmt),
             "summary": event["msg"],
+            "url": event.get("url", ""),
         }
         for event in unformatted_events
     ]
